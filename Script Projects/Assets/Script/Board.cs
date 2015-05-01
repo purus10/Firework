10cefc110a60573d7dffba12f4d001fd63ca81ea
Kostantinos A <protecter_2@hotmail.com>
2015-05-01 17:13:16 -0400

    Added checkers

---
+++ Script Projects/Assets/Script/Board.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+	
+	int[] board = new int[64];
+	// 1 = Player 1 piece, 2 = Player 1 king, 3 = Player 4 piece, 4 = Player 2 king;
+	int saved, enemy, white_pieces = 12, red_pieces = 12, player = 0;
+	string[] piece = new string[7]{"","O","K","O","K","",""};
+	Rect BoardSet(float x, float y, float w, float h, int i)
+	{
+		return new Rect (x + (w * (i % 8)), y + (h * (i/8 % 8)), w, h);
+	}
+	string MarkPlace(string piece, int board_spot)
+	{
+		if (board_spot == 0 || board_spot > 4)
+		{
+			return"";
+		}else return piece;
+	}
+	int MoveCheckRight(int i, int player)
+	{
+		int move = 0;
+		if (board[i] == 2 || board[i] == 3) move = i-7;
+		else  if (board[i] == 1 || board[i] == 4) move = i+7;
+		return move;
+	}
+	int MoveCheckLeft(int i, int player)
+	{
+		int move = 0;
+		if (board[i] == 2 || board[i] == 3) move = i-9;
+		else if (board[i] == 1 || board[i] == 4) move = i+9;
+		return move;
+	}
+	int BoarderCheck(int i, int player)
+	{
+		int move = 0;
+		if (i % 8 == 0) 
+		{
+			if (board[i] == 2 || board[i] == 3) move = i-7;
+			else if (board[i] == 1 || board[i] == 4) move = i+9;
+		} else if (i == 7 || i == 23 || i == 39 || i == 55)
+		{
+			if (board[i] == 2 || board[i] == 3) move = i-9;
+			else if (board[i] == 1 || board[i] == 4) move = i+7;
+		} else move = 0;
+		return move;
+	}
+	//FIX THIS FOR THE KING!!!!
+	/**/
+	int LeftEnemyCheck(int i, int player)
+	{
+		if (player == 1)
+		{
+			if (i-18 > 0 && board[i-9] > 2 && BoarderCheck(board[i-9],player) == 0) return i-18;
+			else return 0;
+		} else{
+			if (i+18 < 63 && board[i+9] == 3 || board[i+9] == 4 && BoarderCheck(board[i+18],player) == 0) return i+18;
+			else return 0;
+		}
+	}
+	int RightEnemyCheck(int i, int player)
+	{
+		if (player == 1)
+		{
+			if (i-14 > 0 && board[i-7] > 1 && BoarderCheck(board[i-7],player) == 0) return i-14;
+			return 0;
+		} else {
+			if (i+14 < 63 && board[i+7] == 3 || board[i+7] == 4 && BoarderCheck(board[i+14],player) == 0) return i+14;
+			return 0;
+		}
+	}
+	/**/
+	int KingCheck(int i, int player)
+	{
+		int change = 0;
+		if (player == 1 && i < 7) change = 4;
+		else if (player == 0 && i > 55) change = 2; 
+		return change;
+	}
+	void Start () 
+	{
+		for (int i = 0;i<board.Length;i++)
+		{
+			if ( i > 0 && i < 8 && i % 2 == 1|| i > 7 && i < 15 && i % 2 == 0 || i > 16 && i < 24 && i % 2 == 1) board[i] = 1;
+			if (i > 39 && i < 48 && i % 2 == 0 /*|| i > 48 && i < 56 && i % 2 == 1 || i > 55 && i < 63 && i % 2 == 0*/) board[i] = 3;
+		}
+	}
+	void Update()
+	{
+		if (player == 0 && red_pieces == 0 || 
+		    player == 1 && white_pieces == 0) print ("GAME OVER");
+	}
+	void OnGUI () {
+		for (int i = 0;i<board.Length;i++)
+		{
+			if (saved == 0 && board[i] > 3) board[i] = 0;
+			if (i > 0 && i < 8 && i % 2 == 1|| i > 7 && i < 15 && i % 2 == 0 ||
+			    i > 16 && i < 24 && i % 2 == 1 ||  i > 23 && i < 31 && i % 2 == 0 ||
+			    i > 32 && i < 40 && i % 2 == 1 || i > 39 && i < 48 && i % 2 == 0 || 
+			    i > 48 && i < 56 && i % 2 == 1 || i > 55 && i < 63 && i % 2 == 0) 
+				GUI.backgroundColor = Color.grey;
+			else GUI.backgroundColor = Color.red;
+			if (board[i] != 0 )
+			{
+				if (board[i] < 3) GUI.contentColor = Color.white;
+				else if (board[i] < 5) GUI.contentColor = Color.red;
+				else GUI.backgroundColor = Color.green;
+			}
+			if (GUI.Button(BoardSet(0,0,50,50,i),MarkPlace(piece[board[i]],board[i])))
+			{
+				if (board[i] != 0)
+				{
+					if (board[i] < 5)
+					{
+						saved = i;
+						if (BoarderCheck(i,player) == 0)
+						{
+							if (board[MoveCheckLeft(i,player)] != 0 && board[LeftEnemyCheck(i,player)] == 0) board[LeftEnemyCheck(i,player)] = 6;
+							else if (board[MoveCheckLeft(i,player)] == 0) board[MoveCheckLeft(i,player)] = 5;
+							if (board[MoveCheckRight(i,player)] != 0 && board[RightEnemyCheck(i,player)] == 0) board[RightEnemyCheck(i,player)] = 6;
+							else if (board[MoveCheckRight(i,player)] == 0) board[MoveCheckRight(i,player)] = 5;
+						}else{
+							if (board[BoarderCheck(i,player)] != 0 && board[LeftEnemyCheck(i,player)] == 0) board[LeftEnemyCheck(i,player)] = 6;
+							if (board[BoarderCheck(i,player)] != 0 && board[RightEnemyCheck(i,player)] == 0) board[LeftEnemyCheck(i,player)] = 6;
+							else if (board[BoarderCheck(i,player)] == 0) board[BoarderCheck(i,player)] = 5;
+						}
+					}
+					if (board[i] == 6)
+					{
+						if (player == 0)
+							if (board[i-18] == 1 || board[i-18] == 2) enemy = i-9;
+						else if (board[i-14] == 1 || board[i-14] == 2) enemy = i-7;
+					}
+					if (board[i] > 3) 
+					{
+						board[i] = board[saved];
+						board[saved] = 0;
+						saved = 0;
+						if (enemy != 0) 
+						{
+							board[enemy] = 0;
+							if (player == 0) red_pieces--;
+							else white_pieces--;
+							print ("RED REMAINING = "+red_pieces+" WHITE REMAINING = "+white_pieces);
+						}
+					}
+					if (KingCheck(i,player) != 0) board[i] = KingCheck(i,player);
+				}
+			}
+		}
+	}
+}
--- Script Projects/Assets/Script/TicTacToe.cs
+++ Script Projects/Assets/Script/TicTacToe.cs
@@ -3,95 +3,93 @@ using System.Collections;
 
 public class TicTacToe : MonoBehaviour {
 	
-	int[,] board = new int[3,3];
-	//int[,] win = new int[,]{{0,1,2},{3,4,5},{8,7,9},{0,3,8},{1,4,7},{2,5,9},{0,4,9},{2,4,8}};
+	int[] board = new int[9];
+	int[][] wins = new int[][]{new int []{0,1,2},new int[]{3,4,5},new int[]{6,7,8},new int[]{0,3,6},new int[]{1,4,7},new int[]{2,5,8},new int[]{0,4,8},new int[]{2,4,6}};
 	public Rect board_but, reset_button, play_button;
 	public int Player_Amount;
 	public string[] Names, Marks = new string[2];
 	public bool Computer;
-	bool set,win;
-	int player; //Tracks number of Players;
-	
-	Rect BoardSet(float x, float y, float w, float h, int j, int i)
+	int player, flow; //Tracks number of Players;
+	Rect BoardSet(float x, float y, float w, float h, int i)
 	{
-		return new Rect (x + (w * j), y + (h * i), w, h);
+		return new Rect (x + (w * (i/3 % 3)), y + (h * (i % 3)), w, h);
 	}
 	string Mark(int b)
 	{
 		if (b != 0) return Marks [b-1];
 		else return "";
 	}
-	string PlayerName(int p,int i, int j)
+	string PlayerName(int p,int i)
 	{
-		return "Player: " + Names[p] + " is playing " + Mark(board[i,j]);
+		return "Player: " + Names[p] + " is playing " + Mark(board[i]);
+	}
+	bool WinCombo()
+	{
+		bool foundWin = false;
+		for (int i = 0; i < wins.Length; i++) 
+		{
+			if (board[wins[i][0]] != 0 && board[wins[i][0]] == board[wins[i][1]] && board[wins[i][0]] == board[wins[i][2]])
+				foundWin = true;
+		}
+		return foundWin;
 	}
 	void getRandomMove () 
 	{
-		if (win == false) 
+		if (flow != 2) 
 		{
-			int i, j, movesLeft = 0;
-			for (i = 0; i < board.GetLength(0); i++) {
-				for (j = 0; j < board.GetLength(1); j++) {
-					if (board [i, j] == 0)
-						movesLeft++;
-				}
+			int i, movesLeft = 0;
+			for (i = 0; i < board.Length; i++) 
+			{
+				if (board [i] == 0)
+					movesLeft++;
 			}
-			for (i = 0; i < board.GetLength(0); i++) {
-				for (j = 0; j < board.GetLength(1); j++) {
-					if (board [i, j] == 0) 
-					{
-						if (movesLeft == 1) SetMark (i, j, player);
-						else movesLeft--;
-					}
-				}
+			for (i = 0; i < board.Length; i++) 
+			{
+				if (board [i] == 0) 
+					if (movesLeft == 1) SetMark (i, player);
+				movesLeft--;
 			}
 		}
 	}
 	void OnGUI () {
-		if (set == false) 
-			if (GUI.Button (play_button, "Play")) set = true;
-		if (set == true) 
+		if (flow == 0) 
+			if (GUI.Button (play_button, "Play")) flow = 1;
+		if (flow != 0) 
+		{
 			if (GUI.Button (reset_button, "Reset")) Reset ();
-		if (set == true) //Initialized and displays Board based on board array collum and rows;
-			for (int i = 0; i < board.GetLength(0); i++) 
-				for (int j = 0; j < board.GetLength(1); j++) 
-					if (GUI.Button (new Rect (BoardSet(board_but.x,board_but.y,board_but.width,board_but.height,i,j)), Mark(board[i,j]))) 
-					{
-					SetMark(i,j,player);
+
+			for (int i = 0; i < board.Length; i++) // intiates board
+				if (GUI.Button (BoardSet(board_but.x,board_but.y,board_but.width,board_but.height,i), Mark(board[i]))) 
+				if (board [i] == 0 && flow != 2) 
+				{
+					SetMark(i,player);
 					if (Computer == true) getRandomMove();
-					CheckCombo ();
-					}
+				} else print ("Choose an empty slot");
+		}
 	}
-	void Update()
+	void FixedUpdate()
 	{
-		if (win)
-		if (Input.anyKeyDown) {
+		if (flow == 2)
+			if (Input.anyKeyDown) 
+		{
 			Reset();
-			set = false;
+			flow = 0;
 		}
 	}
 	void Reset()
 	{
-		for (int i = 0; i< board.GetLength(0); i++) 
-			for (int j = 0; j < board.GetLength(1); j++) board[i,j] = 0;
+		for (int i = 0; i< board.Length; i++) board[i] = 0;
+		player = 0;
 	}
-	void SetMark(int i,int j, int p)
+	void SetMark(int i, int p)
 	{
-		if (board [i,j] == 0) {
-			board [i,j] = 1 + (player % Marks.Length);
-			print (PlayerName (p,i,j));
-			player = (p+1) % Player_Amount;
-		} else print ("Choose an empty slot");
-	}
-	void CheckCombo()
-	{
-		for (int j = 0; j < board.GetLength(0); j++) 
+		board [i] = 1 + (player % Marks.Length);
+		print (PlayerName (p,i));
+		player = (p+1) % Player_Amount;
+		if (WinCombo() == true)
 		{
-			if (board[j,0] == board[j,1] && board[j,1] == board[j,2] && board[j,0] != 0) win = true;
-			else if (board[0,j] == board[1,j] && board[1,j] == board[2,j] && board[0,j] != 0) win = true;
-			else if (board[0,0] == board[1,1] && board[1,1] == board[2,2] && board[0,0] != 0) win = true;
-			else if (board[0,2] == board[1,1] && board[1,1] == board[2,0] && board[0,2] != 0) win = true;
-			if (win == true) print ("win!");
+			print ("Win!");
+			flow = 2;
 		}
 	}
 }
